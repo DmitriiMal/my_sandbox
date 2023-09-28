4f62543a2e3a8f2c57c7256c895716aa343eba41
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-09-28 21:48:08 +0200

    test

--- JS/functions.js
+++ JS/functions.js
@@ -1,127 +1,127 @@
-// /////////////////// //
-// //// Functions //// //
-// /////////////////// //
+// // /////////////////// //
+// // //// Functions //// //
+// // /////////////////// //
 
 
 
 
-function product(name, price) {
-  return `The product: ${name} costs € ${price}`;
-}
-console.log(product("Bread", 1.99));
+// function product(name, price) {
+//   return `The product: ${name} costs € ${price}`;
+// }
+// console.log(product("Bread", 1.99));
 
 
 
-///
-function getRasult(numberOne, numberTwo, NumberThree) {
-  let sum = numberOne + numberTwo + NumberThree;
-  let multiply = numberOne * numberTwo * NumberThree;
-  let result = [sum, multiply];
-  return result;
-}
+// ///
+// function getRasult(numberOne, numberTwo, NumberThree) {
+//   let sum = numberOne + numberTwo + NumberThree;
+//   let multiply = numberOne * numberTwo * NumberThree;
+//   let result = [sum, multiply];
+//   return result;
+// }
 
-console.log(getRasult(2, 8, 4)[0]);
-
-
-// =>
-
-///
-const multiply = (numb1, numb2) => {
-  return numb1 * numb2;
-}
-
-console.log(multiply(2, 4));
+// console.log(getRasult(2, 8, 4)[0]);
 
 
-///
-const sum = nmb => nmb + nmb;
+// // =>
 
-console.log(sum(9));
+// ///
+// const multiply = (numb1, numb2) => {
+//   return numb1 * numb2;
+// }
 
-
-
-///
-const cl = smt => console.log(smt);
-
-cl('something');
-
-///
+// console.log(multiply(2, 4));
 
 
-let randomNum = Math.floor(Math.random() * 110);
+// ///
+// const sum = nmb => nmb + nmb;
 
-console.log(randomNum);
+// console.log(sum(9));
 
 
 
-///
+// ///
+// const cl = smt => console.log(smt);
 
-let today = new Date();
-console.log(today);
-alert(today);
+// cl('something');
 
-document.querySelector('h1').innerHTML = today;
+// ///
 
-///
-document.write("The result from multiplying 5 to 78 is " + (5 * 78));
 
-document.write("<h1>This is a H1 title</h1> ");
+// let randomNum = Math.floor(Math.random() * 110);
 
+// console.log(randomNum);
 
 
 
+// ///
 
-// default parameter
+// let today = new Date();
+// console.log(today);
+// alert(today);
 
-function register(user = 'User') {
-  return `${user} is registered`
-}
+// document.querySelector('h1').innerHTML = today;
 
-console.log(register());
+// ///
+// document.write("The result from multiplying 5 to 78 is " + (5 * 78));
+
+// document.write("<h1>This is a H1 title</h1> ");
 
 
 
 
-// Rest params
 
-function sumOfNumbers(...numbers) {
+// // default parameter
 
-  let total = 0;
-  for (const num of numbers) {
-    total += num;
-  };
-  return total;
-}
+// function register(user = 'User') {
+//   return `${user} is registered`
+// }
 
-console.log(sumOfNumbers(1, 2, 3, 4, 5));
-
-
-// Object as a Function
-
-const user1 = {
-  name: "Dmitrii",
-  id: 12349876,
-}
-
-function logIn(user) {
-  messege = `${user.name} with id: ${user.id} has logged in`
-  return messege;
-}
-
-console.log(logIn(user1));
+// console.log(register());
 
 
 
-// Array as a Function
 
-const myArray = [1, 2, 3, 4, 5, 6, 7];
+// // Rest params
 
-// or // function getRandom(...arr) {
-function getRandom(arr) {
-  const randomInd = Math.floor(Math.random() * arr.length);
+// function sumOfNumbers(...numbers) {
 
-  return arr[randomInd];
-}
+//   let total = 0;
+//   for (const num of numbers) {
+//     total += num;
+//   };
+//   return total;
+// }
 
-console.log(getRandom(myArray));
+// console.log(sumOfNumbers(1, 2, 3, 4, 5));
+
+
+// // Object as a Function
+
+// const user1 = {
+//   name: "Dmitrii",
+//   id: 12349876,
+// }
+
+// function logIn(user) {
+//   messege = `${user.name} with id: ${user.id} has logged in`
+//   return messege;
+// }
+
+// console.log(logIn(user1));
+
+
+
+// // Array as a Function
+
+// const myArray = [1, 2, 3, 4, 5, 6, 7];
+
+// // or // function getRandom(...arr) {
+// function getRandom(arr) {
+//   const randomInd = Math.floor(Math.random() * arr.length);
+
+//   return arr[randomInd];
+// }
+
+// console.log(getRandom(myArray));
 
