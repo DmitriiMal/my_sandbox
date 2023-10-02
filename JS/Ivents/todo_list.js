2a72e50ac97fe86b4ea2adcd09d0ec78a4935c76
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-10-02 23:15:57 +0200

    To Do List

--- JS/Ivents/todo_list.js
+++ JS/Ivents/todo_list.js
@@ -3,3 +3,30 @@
 // //////////////////////////// //
 
 
+// setTimeout(()=>{alert("Hello world!")},3000);
+
+
+function createTask() {
+  let newTask = document.getElementById('demo').value;
+
+  document.getElementById('list').innerHTML += `<li>${newTask} </li>`
+  addRemoveToList();
+}
+
+document.getElementById('create').addEventListener('click', createTask);
+
+
+function removeItem() {
+  this.style.opacity = '0';
+  this.style.transition = '1s';
+  this.style.position = 'absolute';
+  this.style.left = '-100px';
+}
+
+
+function addRemoveToList() {
+  let items = document.getElementsByTagName('li');
+  for (let i in items) {
+    items[i].addEventListener('click',removeItem);
+  }
+}
