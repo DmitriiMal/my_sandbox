284441bc10e8485789c351e521a285d87bcfd83e
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2024-02-23 09:32:56 +0100

    scroll to top button

--- yavin-website/js/script.js
+++ yavin-website/js/script.js
@@ -11,12 +11,15 @@ function isInViewport(element) {
 
 function userScroll() {
   const navbar = document.querySelector('.navbar');
+  const toTopBtn = document.querySelector('#to-top');
 
   window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
       navbar.classList.add('navbar-sticky');
+      toTopBtn.classList.add('show');
     } else {
       navbar.classList.remove('navbar-sticky');
+      toTopBtn.classList.remove('show');
     }
   });
 
@@ -25,6 +28,12 @@ function userScroll() {
       incrementStats();
     }
   });
+  toTopBtn.addEventListener('click', scrollToTop);
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 // Increment Stats
